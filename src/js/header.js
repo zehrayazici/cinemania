da604298bf1cb147a5ee4700ff80edc68284671b
Cigdem Ergal <cigdemtahtasiz@gmail.com>
2026-01-29 13:11:33 +0300

    feat: header section completed with mobile-first design and theme toggle

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,46 @@
-export function initHeader() {}
+import { initHeader } from './js/header.js';
+
+// Sayfa yüklendiğinde header fonksiyonunu çalıştır
+document.addEventListener('DOMContentLoaded', () => {
+    initHeader();
+});
+
+
+
+export function initHeader() {
+    const themeSwitch = document.getElementById('theme-switch');
+    const body = document.body;
+
+    // tema kontrolü(varsayılan dark ve localStorage)
+    const currentTheme = localStorage.getItem('theme') || 'dark';
+    body.classList.add(`${currentTheme}-theme`);
+
+    if (currentTheme === 'light') {
+        themeSwitch.checked = true;
+    }
+
+    // tema değişikliği
+
+    themeSwitch.addEventListener('change', () => {
+        if (themeSwitch.checked) {
+            body.classList.replace('dark-theme', 'light-theme');
+            localStorage.setItem('theme', 'light');
+        } else {
+            body.classList.replace('light-theme', 'dark-theme');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+
+    //// 2. Aktif Sayfa Vurgulama Mantığı
+  const navLinks = document.querySelectorAll('.nav-link');
+  const currentPath = window.location.pathname;
+
+  navLinks.forEach(link => {
+    if (link.getAttribute('href').includes(currentPath) && currentPath !== '/') {
+      link.classList.add('active');
+    } else if (currentPath === '/' || currentPath.includes('index.html')) {
+      document.querySelector('#home-link')?.classList.add('active');
+    }
+  });
+}
+
