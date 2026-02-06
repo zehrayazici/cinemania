4c0c747137d090c0e1bc3284b634c9f0e0b8def8
Burak Gökay <b.gokay007@gmail.com>
2026-02-06 21:16:34 +0300

    fix: resolved merge conflict in movies-api

--- src/js/api/movies-api.js
+++ src/js/api/movies-api.js
@@ -2,12 +2,6 @@
  export const API_KEY = '6e2b9ceff6313b472d60eed2769db38a'; // benim anahtar
  export const BASE_URL = 'https://api.themoviedb.org/3';
  export const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
-/**
- * Haftalık trend olan filmleri getirir.
- */
- export const API_KEY = '6e2b9ceff6313b472d60eed2769db38a'; // benim anahtar
- export const BASE_URL = 'https://api.themoviedb.org/3';
- export const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
 
 //Haftalık trend olan filmleri getirir.
 
